8724513ca1881d310dee06d4f5adf7db1776bc27
TLOYYBCm <86084516+TLOYYBCm@users.noreply.github.com>
2021-06-20 20:19:36 +0200

    Update my.js

--- my.js
+++ my.js
@@ -24,7 +24,7 @@ function addGS() {
         },
         redirect: 'follow', // manual, *follow, error
         //referrerPolicy: 'no-referrer', // no-referrer, *no-referrer-when-downgrade, origin, origin-when-cross-origin, same-origin, strict-origin, strict-origin-when-cross-origin, unsafe-url
-        body: JSON.stringify({name:"Olivia2"}) // body data type must match "Content-Type" header
+        body: JSON.stringify({First:"Thierry",Last:"LOYUAI",Telephone:"123-865-234"}) // body data type must match "Content-Type" header
       });
 }
 
